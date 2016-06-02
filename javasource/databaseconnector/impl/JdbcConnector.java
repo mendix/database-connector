387030a91abf11b9c4a8c924c2138b53afbab40e
Jonathan Veldhuizen <jonathan.veldhuizen@mendix.nl>
2016-06-02 16:51:21 +0200

    Make executeQuery implementation more generic.

--- javasource/databaseconnector/impl/JdbcConnector.java
+++ javasource/databaseconnector/impl/JdbcConnector.java
@@ -7,6 +7,7 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import com.mendix.logging.ILogNode;
 import com.mendix.systemwideinterfaces.core.IContext;
@@ -33,6 +34,24 @@ public class JdbcConnector {
   public List<IMendixObject> executeQuery(String jdbcUrl, String userName, String password, String entityName, String sql,
       IContext context) throws SQLException {
     List<IMendixObject> resultList = new ArrayList<>();
+
+    executeQuery(jdbcUrl, userName, password, sql, context, row -> {
+      IMendixObject obj = objectInstantiator.instantiate(context, entityName);
+      row.stream().forEach(c -> obj.setValue(context, c.name, c.value));
+      resultList.add(obj);
+      logNode.info("obj: " + obj);
+    });
+
+    logNode.info(String.format("List: %d", resultList.size()));
+    return resultList;
+  }
+
+  public String executeQueryToJson(String jdbcUrl, String userName, String password, String sql, IContext context) throws SQLException {
+    return null;
+  }
+
+  public void executeQuery(String jdbcUrl, String userName, String password, String sql,
+      IContext context, Consumer<List<Column>> consumer) throws SQLException {
     logNode.info(String.format("executeQuery: %s, %s, %s", jdbcUrl, userName, sql));
 
     // TODO: make sure user doesn't crash runtime due to retrieving too many records
@@ -43,23 +62,28 @@ public class JdbcConnector {
       int columnCount = resultSetMetaData.getColumnCount();
 
       while (resultSet.next()) {
-        IMendixObject obj = objectInstantiator.instantiate(context, entityName);
+        ArrayList<Column> row = new ArrayList<Column>();
+
         for (int i = 0; i < columnCount; i++) {
           String columnName = resultSetMetaData.getColumnName(i + 1);
           Object columnValue = resultSet.getObject(i + 1);
           logNode.info(String.format("setting col: %s = %s", columnName, columnValue));
-          obj.setValue(context, columnName, columnValue);
+          row.add(new Column(columnName, columnValue));
         }
-        resultList.add(obj);
-        logNode.info("obj: " + obj);
+
+        consumer.accept(row);
       }
     }
-    logNode.info(String.format("List: %d", resultList.size()));
-    return resultList;
   }
 
-  public String executeQueryToJson(String jdbcUrl, String userName, String password, String sql, IContext context) throws SQLException {
-    return null;
+  public class Column {
+    public String name;
+    public Object value;
+
+    public Column(String name, Object value) {
+      this.name = name;
+      this.value = value;
+    }
   }
 
   public long executeStatement(String jdbcUrl, String userName, String password, String sql) throws SQLException {
