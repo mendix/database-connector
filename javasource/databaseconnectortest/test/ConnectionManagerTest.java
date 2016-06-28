83f841d513ae7db1bff46bd91f8499a9c98829f2
Jonathan Veldhuizen <jonathan.veldhuizen@mendix.nl>
2016-06-28 11:41:51 +0200

    Re-enable connection test case.

--- javasource/databaseconnectortest/test/ConnectionManagerTest.java
+++ javasource/databaseconnectortest/test/ConnectionManagerTest.java
@@ -4,19 +4,16 @@ import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import com.mendix.logging.ILogNode;
-import com.mendix.logging.impl.LogManager;
 
 import databaseconnector.impl.JdbcConnectionManager;
 import databaseconnector.interfaces.ConnectionManager;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Matchers;
-import org.mockito.Mockito;
-import org.slf4j.impl.StaticLoggerBinder;
-
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class ConnectionManagerTest {
   private static final String jdbcUrl = "jdbc:hsqldb:mem:testcase;shutdown=true";
@@ -30,25 +27,25 @@ public class ConnectionManagerTest {
 //  @Before
 //  public void setUp() {
 //    ILogNode logNode = Mockito.mock(ILogNode.class);
-//    LogManager logManager = Mockito.mock(LogManager.class);
+//    ILogManager logManager = Mockito.mock(ILogManager.class);
 //    Mockito.when(logManager.getLogNode(Matchers.anyString())).thenReturn(logNode);
 //    StaticLoggerBinder.init(logManager);
 //  }
-//
-//  @Test
-//  public void testGetConnection() throws SQLException {
-//    ILogNode logger = Mockito.mock(ILogNode.class);
-//    ConnectionManager manager = newConnManager(logger);
-//
-//    Connection conn1 = manager.getConnection(jdbcUrl, userName, password);
-//    Mockito.verify(logger, times(3)).info(anyString()); // One log message is printed in JdbcConnectionManager.initializeDrivers.
-//    conn1.close();
-//
-//    Connection conn2 = manager.getConnection(jdbcUrl, userName, password);
-//    Mockito.verify(logger, times(4)).info(anyString());
-//    conn2.close();
-//
-//    assertNotEquals(conn1, conn2);
-//  }
+
+  @Test
+  public void testGetConnection() throws SQLException {
+    ILogNode logger = Mockito.mock(ILogNode.class);
+    ConnectionManager manager = newConnManager(logger);
+
+    Connection conn1 = manager.getConnection(jdbcUrl, userName, password);
+    Mockito.verify(logger, times(3)).trace(anyString()); // One log message is printed in JdbcConnectionManager.initializeDrivers.
+    conn1.close();
+
+    Connection conn2 = manager.getConnection(jdbcUrl, userName, password);
+    Mockito.verify(logger, times(4)).trace(anyString());
+    conn2.close();
+
+    assertNotEquals(conn1, conn2);
+  }
 
 }
