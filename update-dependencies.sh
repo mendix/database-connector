924dfce5f33133be7acead22ad533dbfdec2d1d0
Augusto Passalaqua <Augusto.Passalaqua@mendix.com>
2020-04-03 13:40:02 +0200

    Remove old jdbc libs, add pom.xml and script to update them in the future.

---
+++ update-dependencies.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env sh
+
+# First download and copy the new jdbc drivers to userlib
+mvn dependency:copy-dependencies@userlib-copy
+
+# Then create remove the old RequiredLib files:
+rm -f userlib/*DatabaseConnector.RequiredLib
+
+# Recreate those RequiredLib files:
+mvn dependency:list \
+    | sed -ne 's#[^\s]*    \(.*\):\(.*\):jar:\(.*\):runtime#userlib/\1.\2-\3.jar.DatabaseConnector.RequiredLib#p' \
+    | xargs touch
+
+# Update the license report file
+mvn project-info-reports:dependencies
+
+# Create a neat zip file with that information
+cd target/site
+zip -r ../../userlib/dependency-info.zip .
+cd ../../
+
+# Clean up
+mvn clean
