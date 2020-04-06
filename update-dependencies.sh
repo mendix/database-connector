81d847899912d081e7207c2b62fe3b507da2935c
Augusto Passalaqua <Augusto.Passalaqua@mendix.com>
2020-04-06 17:45:22 +0200

    Add DatabaseConnectorTest dependencies to pom.xml, split pom.xml.

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -5,14 +5,19 @@ mvn dependency:copy-dependencies@userlib-copy
 
 # Then create remove the old RequiredLib files:
 rm -f userlib/*DatabaseConnector.RequiredLib
+rm -f userlib/*DatabaseConnectorTest.RequiredLib
 
 # Recreate those RequiredLib files:
-mvn dependency:list \
+mvn dependency:list -pl :databaseconnector \
     | sed -nE 's#[^\s]*    (.*):(.*):jar:(.*):(compile|runtime)#userlib/\1.\2-\3.jar.DatabaseConnector.RequiredLib#p' \
     | xargs touch
 
+mvn dependency:list -pl :databaseconnectortest \
+    | sed -nE 's#[^\s]*    (.*):(.*):jar:(.*):(compile|runtime)#userlib/\1.\2-\3.jar.DatabaseConnectorTest.RequiredLib#p' \
+    | xargs touch
+
 # Update the license report file
-mvn project-info-reports:dependencies
+mvn project-info-reports:dependencies -pl :databaseconnector
 
 # Create a neat zip file with that information
 cd target/site
