c3b6b23f9637004549e27067cc733c982917e8e1
Augusto Passalacqua <Augusto.Passalacqua@mendix.com>
2021-06-17 18:02:32 +0200

    Move SqlParameter initializer/factory to its interface file.

--- javasource/databaseconnector/impl/CallableStatementCreatorImpl.java
+++ javasource/databaseconnector/impl/CallableStatementCreatorImpl.java
@@ -8,23 +8,11 @@ import java.util.stream.Collectors;
 
 import com.mendix.core.Core;
 import com.mendix.systemwideinterfaces.core.IContext;
-import com.mendix.systemwideinterfaces.core.IMendixObject;
 
 import databaseconnector.impl.callablestatement.SqlParameter;
-import databaseconnector.impl.callablestatement.SqlParameterDatetime;
-import databaseconnector.impl.callablestatement.SqlParameterDecimal;
-import databaseconnector.impl.callablestatement.SqlParameterLong;
-import databaseconnector.impl.callablestatement.SqlParameterObject;
-import databaseconnector.impl.callablestatement.SqlParameterPrimitiveValue;
-import databaseconnector.impl.callablestatement.SqlParameterString;
 import databaseconnector.impl.callablestatement.StatementWrapper;
 import databaseconnector.interfaces.CallableStatementCreator;
 import databaseconnector.proxies.Parameter;
-import databaseconnector.proxies.ParameterDatetime;
-import databaseconnector.proxies.ParameterDecimal;
-import databaseconnector.proxies.ParameterLong;
-import databaseconnector.proxies.ParameterObject;
-import databaseconnector.proxies.ParameterString;
 import databaseconnector.proxies.Statement;
 
 public class CallableStatementCreatorImpl implements CallableStatementCreator {
@@ -37,42 +25,12 @@ public class CallableStatementCreatorImpl implements CallableStatementCreator {
 		List<SqlParameter> parameters = Core
 				.retrieveByPath(context, statement.getMendixObject(),
 						Parameter.MemberNames.Parameter_Statement.toString())
-				.stream().map(p -> initialize(context, p)).collect(Collectors.toList());
+				.stream().map(p -> SqlParameter.initialize(context, p)).collect(Collectors.toList());
 
 		for (SqlParameter p : parameters) {
 			p.prepareCall(cStatement);
 		}
 
 		return new StatementWrapper(cStatement, parameters);
-	}
-
-	public SqlParameter initialize(final IContext context, IMendixObject mendixObject) {
-		SqlParameter ret = null;
-
-		switch (mendixObject.getType()) {
-		case ParameterDatetime.entityName:
-			ret = new SqlParameterDatetime(context, mendixObject);
-			break;
-		case ParameterString.entityName:
-			ret = new SqlParameterString(context, mendixObject);
-			break;
-		case ParameterLong.entityName:
-			ret = new SqlParameterLong(context, mendixObject);
-			break;
-		case ParameterDecimal.entityName:
-			ret = new SqlParameterDecimal(context, mendixObject);
-			break;
-		case ParameterObject.entityName:
-			List<SqlParameterPrimitiveValue<?>> fields = Core
-					.retrieveByPath(context, mendixObject, Parameter.MemberNames.MemberOfObject.toString(), true)
-					.stream().map(p -> (SqlParameterPrimitiveValue<?>) initialize(context, p)).sorted()
-					.collect(Collectors.toList());
-			ret = new SqlParameterObject(context, mendixObject, fields);
-			break;
-		default:
-			throw new IllegalArgumentException(
-					String.format("Parameter type %s not supported.", mendixObject.getType()));
-		}
-		return ret;
 	}
 }
--- javasource/databaseconnector/impl/callablestatement/SqlParameter.java
+++ javasource/databaseconnector/impl/callablestatement/SqlParameter.java
@@ -2,11 +2,53 @@ package databaseconnector.impl.callablestatement;
 
 import java.sql.CallableStatement;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.mendix.core.Core;
+import com.mendix.systemwideinterfaces.core.IContext;
+import com.mendix.systemwideinterfaces.core.IMendixObject;
 
 import databaseconnector.impl.DatabaseConnectorException;
+import databaseconnector.proxies.Parameter;
+import databaseconnector.proxies.ParameterDatetime;
+import databaseconnector.proxies.ParameterDecimal;
+import databaseconnector.proxies.ParameterLong;
+import databaseconnector.proxies.ParameterObject;
+import databaseconnector.proxies.ParameterString;
 
 public interface SqlParameter {
 	void prepareCall(CallableStatement cStatement) throws SQLException;
 
 	void retrieveResult(CallableStatement cStatement) throws SQLException, DatabaseConnectorException;
+
+	public static SqlParameter initialize(final IContext context, IMendixObject mendixObject) {
+		SqlParameter ret = null;
+
+		switch (mendixObject.getType()) {
+		case ParameterDatetime.entityName:
+			ret = new SqlParameterDatetime(context, mendixObject);
+			break;
+		case ParameterString.entityName:
+			ret = new SqlParameterString(context, mendixObject);
+			break;
+		case ParameterLong.entityName:
+			ret = new SqlParameterLong(context, mendixObject);
+			break;
+		case ParameterDecimal.entityName:
+			ret = new SqlParameterDecimal(context, mendixObject);
+			break;
+		case ParameterObject.entityName:
+			List<SqlParameterPrimitiveValue<?>> fields = Core
+					.retrieveByPath(context, mendixObject, Parameter.MemberNames.MemberOfObject.toString(), true)
+					.stream().map(p -> (SqlParameterPrimitiveValue<?>) initialize(context, p)).sorted()
+					.collect(Collectors.toList());
+			ret = new SqlParameterObject(context, mendixObject, fields);
+			break;
+		default:
+			throw new IllegalArgumentException(
+					String.format("Parameter type %s not supported.", mendixObject.getType()));
+		}
+		return ret;
+	}
 }
